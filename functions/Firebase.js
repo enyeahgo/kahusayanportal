047944947d3d4924238ed3743ac81aab2ca5104b
Inigo Orosco II <enyeahgo@gmail.com>
2020-05-31 00:13:10 +0800

    Removed credentials from Firebase.js

--- functions/Firebase.js
+++ functions/Firebase.js
@@ -2,14 +2,14 @@ const firebase = require('firebase');
 require('firebase/auth');
 
 const config = {
-    apiKey: "AIzaSyBHJk6Z4bbkQIB0BSn4rRdewvVzCDnm9_o",
-    authDomain: "kahusayanportal.firebaseapp.com",
-    databaseURL: "https://kahusayanportal.firebaseio.com",
-    projectId: "kahusayanportal",
-    storageBucket: "kahusayanportal.appspot.com",
-    messagingSenderId: "162435559210",
-    appId: "1:162435559210:web:c0a1a439945663bdebf853",
-    measurementId: "G-QJLHTVMMFC"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 firebase.initializeApp(config);
